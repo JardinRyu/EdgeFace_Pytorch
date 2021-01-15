68c85aa5ac095b1f4adb7365da2449a9fb847ff1
Jeong Won Ryu <54166852+JardinRyu@users.noreply.github.com>
2021-01-15 15:01:11 +0900

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,18 +1,18 @@
 # config.py
 
 cfg_EdgeFace = {
-    #'name': 'EdgeFacenet',
+    'name': 'EdgeFacenet',
     'min_sizes': [[16, 32], [64, 128], [256, 512]],
     'steps': [8, 16, 32],
     'variance': [0.1, 0.2],
     'clip': False,
     'loc_weight': 2.0,
     'gpu_train': True,
-    'batch_size': 4,
+    'batch_size': 10,
     'ngpu': 1,
-    'epoch': 100,
-    'decay1': 50,
-    'decay2': 68,
+    'epoch': 200,
+    'decay1': 135,
+    'decay2': 170,
     'image_size': 640,
     'pretrain': False,
     'return_layers': {'stage1': 1, 'stage2': 2, 'stage3': 3},
